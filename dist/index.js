752db41756464335b9e75411478253d1d691d12e
Diogo Paulos <51181228+dpaulos6@users.noreply.github.com>
2024-03-01 21:21:26 +0000

    Project update
    Commander lib working

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,19 @@
-"use strict"
-console.log("Testing CLI")
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
+// Create a new instance of Commander
+const program = new commander_1.Command();
+// Define the version of your CLI tool
+program.version('1.0.0');
+// Define a command with options and description
+program
+    .command('greet <name>')
+    .description('Greet a user')
+    .option('-t, --title <title>', 'Title for the greeting')
+    .action((name, options) => {
+    const title = options.title ? options.title : 'Hello';
+    console.log(`${title}, ${name}!`);
+});
+// Parse command line arguments
+program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Testing CLI")
+import { Command } from 'commander';
+const program = new Command();
+
+program.version('1.0.0');
+
+program
+  .command('greet <name>')
+  .description('Greet a user')
+  .option('-t, --title <title>', 'Title for the greeting')
+  .action((name, options) => {
+    const title = options.title ? options.title : 'Hello';
+    console.log(`${title}, ${name}!`);
+  });
+
+program.parse(process.argv);
